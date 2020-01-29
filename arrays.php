b489f393f1c2dc1aa9ace4087d5bf060e947e452
Preetjyot Baweja <59571117+preetjyotbaweja@users.noreply.github.com>
2020-01-28 23:57:06 -0500

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,192 @@
+<?php /*
++---+
+| 1 |
++---+
+Declare and assign the indexed array with your favourite food 
+(at least 4 array elements). Name the array food.
+*/
+
+$food = [
+"Rice",
+"Eggs",
+"Soups",
+"Burger"];
+
+/*
+Print every array element in a new line.
+*/
+
+echo $food[0] . "<br>";
+echo $food[1] . "<br>";
+echo $food[2] . "<br>";
+echo $food[3] . "<br>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 2 |
++---+
+Print the array elements from the previous exercise in unordered list.
+<ul>
+  <li>...</li>
+  <li>...</li>
+  <li>...</li>
+  <li>...</li>
+</ul>
+*/
+
+echo "<ul>
+<li>" . $food[0] . "</li>"
+ . "<li>" . $food[1] . "</li>"
+ . "<li>" . $food[2] . "</li>"
+ . "<li>" . $food[3] . "</li>
+ </ul>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 3 |
++---+
+Turn the indexed array food from task 1 into associative array named food_assoc. 
+Every array element of food becomes the key of food_assoc.
+Every key of food_assoc has the value that describes the type of food (salad, main course or dessert).
+*/
+
+$food_assoc = [
+"Rice"=>"MainCourse",
+"Eggs"=>"Breakfast",
+"Soups"=>"Starter",
+"Burger"=>"Snack"];
+
+/*
+Print every food and type in the separate lines so it renders like this:
+pizza | main counrse 
+cheesesake | desert 
+*/
+
+echo $food[0] . " | " . $food_assoc["Rice"] . "<br>" 
+ . $food[1] . " | " . $food_assoc["Eggs"] . "<br>"
+ . $food[2] . " | " . $food_assoc["Soups"] . "<br>"
+ . $food[3] . " | " . $food_assoc["Burger"] . "<br>";
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 4 |
++---+
+Break down the food_assoc from the task 3 to type (salad, main course, dessert, ...) 
+and country of origin (Italy, Spain, India, ...)
+food_assoc:
+  pizza:
+    type: main course
+    origin: Italy
+  cheesesake: 
+    type:desert
+    origin: Greece
+So, food_assoc is still going to be an associative array. 
+Every key of food_assoc (pizza, cheesecake) will now have the value that is associative array itself 
+and carries the information about the type and origin
+*/
+
+$food_assoc = [
+  "Rice"=> [
+    "type" => "MainCourse",
+    "origin" => "India"
+  ],
+  "Eggs"=> [
+    "type" => "Breakfast", 
+    "origin" => "England"
+  ],
+  "Soups"=>[
+    "type" => "Starter", 
+    "origin" => "India"
+  ],
+  "Burger"=>[
+    "type" => "Snack", 
+    "origin" => "America"
+  ]
+];
+
+/*
+Print every food, type and origin in the separate lines so it renders like this:
+pizza | main counrse | Italy
+cheesesake | desert | Greece
+*/
+echo $food[0]
+ . " | "
+ . $food_assoc["Rice"]["type"]
+ . " | "
+ . $food_assoc["Rice"]["origin"]
+ . "<br>" . $food[1]
+ . " | "
+ . $food_assoc["Eggs"]["type"]
+ . " | "
+ . $food_assoc["Eggs"]["origin"]
+ . "<br>" . $food[2]
+ . " | "
+ . $food_assoc["Soups"]["type"]
+ . " | "
+ . $food_assoc["Soups"]["origin"]
+ . "<br>" . $food[3]
+ . " | "
+ . $food_assoc["Burger"]["type"]
+ . " | "
+ . $food_assoc["Burger"]["origin"]
+;
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 5 |
++---+
+Print the array from task 4 in html table:
+<table>
+  <tr>
+    <th>food</th>
+    <th>type</th>
+    <th>origin</th>
+  </tr>
+  <tr>
+    <td>pizza</td>
+    <td>main course</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>cheesecake</td>
+    <td>desert</td>
+    <td>Greece</td>
+  </tr>
+</table>
+*/
+echo
+"<table>
+  <tr>
+    <th>".$food[0]."</th>
+    <th>".$food_assoc["Rice"]["type"]."</th>
+    <th>".$food_assoc["Rice"]["origin"]."</th>
+  </tr>
+  <tr>
+    <th>".$food[1]."</th>
+    <th>".$food_assoc["Eggs"]["type"]."</th>
+    <th>".$food_assoc["Eggs"]["origin"]."</th>
+  </tr>
+  <tr>
+    <th>".$food[2]."</th>
+    <th>".$food_assoc["Soups"]["type"]."</th>
+    <th>".$food_assoc["Soups"]["origin"]."</th>
+  </tr>
+  <tr>
+    <th>".$food[3]."</th>
+    <th>".$food_assoc["Burger"]["type"]."</th>
+    <th>".$food_assoc["Burger"]["origin"]."</th>
+  </tr>
+</table>"
+?>
